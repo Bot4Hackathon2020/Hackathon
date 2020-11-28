082e309e8c517e330b51a39abf33ca9bdf95bb73
TF-Liu <55623347+TF-Liu@users.noreply.github.com>
2020-11-28 20:33:42 +0800

    Add files via upload

--- QAQ/mainwindow.cpp
+++ QAQ/mainwindow.cpp
@@ -6,12 +6,6 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    myPath = "D:大二上";
-    QDir dir(myPath);
-    QFileInfoList list;
-
-    list = dir.entryInfoList();
-    showFileInfoList(list);
 }
 
 MainWindow::~MainWindow()
@@ -21,6 +15,11 @@ MainWindow::~MainWindow()
 
 
 void MainWindow::on_action_triggered()
+{
+    QMessageBox::information(this, "消息", "选择了打开菜单项");
+}
+
+void MainWindow::on_action_2_triggered()
 {
     QMessageBox::information(this, "消息", "选择了打开菜单项");
 }
@@ -36,60 +35,10 @@ void MainWindow::on_pushButton_2_clicked()
     close();
 }
 
-void MainWindow::on_listWidget_itemDoubleClicked(QListWidgetItem *item)
+void MainWindow::on_pushButton_3_clicked()
 {
-    connect(ui->listWidget,SIGNAL(itemDoubleClicked(QListWidgetItem*)),
-                    this,SLOT(slotDirShow(QListWidgetItem*)));
-}
-
-void MainWindow::showFileInfoList(QFileInfoList list)
-{
-    ui->listWidget->clear();
-    for(int i=0; i<list.count() ;i++)
-    {
-        QFileInfo tmpFileInfo = list.at(i);
-        if(tmpFileInfo.isDir())    //为目录则添加文件夹图标及对应文件名
-        {
-            QIcon icon(":/1.jpg");
-            QString fileName = tmpFileInfo.fileName();
-            QListWidgetItem *tmp = new QListWidgetItem(icon, fileName);
-            ui->listWidget->addItem(tmp);
-        }
-        else if(tmpFileInfo.isFile())    //为普通文件则添加普通文件图标及对应文件名
-        {
-            QIcon icon(":/2.jpg");
-            QString fileName = tmpFileInfo.fileName();
-            QListWidgetItem *tmp = new QListWidgetItem(icon, fileName);
-            ui->listWidget->addItem(tmp);
-        }
-    }
-}
-
-void MainWindow::slotShow(QDir dir)
-{
-    QFileInfoList list = dir.entryInfoList();
-    showFileInfoList(list);
-}
-
-//鼠标双击触发，接受item参数
-void MainWindow::slotDirShow(QListWidgetItem *item)
-{
-    QString str = item->text();
-    QDir dir;
-
-    dir.setPath(myPath);
-    if(!dir.cd(str))         //如果进入失败，则为普通文件，创建新的进程打开对应文件
-    {
-        QStringList arguments;
-        QString start_path = myPath+"\\" + str;
-        arguments << "/c" << start_path;
-
-        QProcess* process = new QProcess;
-
-        qDebug() << start_path << endl;
-        process->start("cmd.exe",arguments);  //开启新进程打开文件
+    QString fileName = QFileDialog::getOpenFileName(this,tr("选择文件"),tr(":/"),tr("*.*"));
+    QFile myFile(fileName);
+    if(!myFile.open(QIODevice::ReadOnly | QIODevice::Text))
         return;
-    }
-    myPath = dir.absolutePath();    //更新路径
-    slotShow(dir);
 }
--- QAQ/mainwindow.h
+++ QAQ/mainwindow.h
@@ -3,12 +3,8 @@
 
 #include <QMainWindow>
 #include <QMessageBox>
-#include <QDir>
-#include <QFileInfoList>
-#include <QFileInfo>
-#include <QProcess>
-#include <QListWidgetItem>
-#include <QDebug>
+#include <QString>
+#include <QFileDialog>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -29,16 +25,11 @@ private slots:
 
     void on_pushButton_2_clicked();
 
-    void on_listWidget_itemDoubleClicked(QListWidgetItem *item);
+    void on_pushButton_3_clicked();
+
+    void on_action_2_triggered();
 
 private:
     Ui::MainWindow *ui;
-
-    void showFileInfoList(QFileInfoList list);  //根据获取的QFileInfoList显示本地目录
-    void slotDirShow(QListWidgetItem *item);    //由双击的文件确定新的目录
-    void slotShow(QDir dir);                    //由新的目录调用showFileInfoList显示
-
-    private:
-      QString myPath;   //记录文件当前路径
 };
 #endif // MAINWINDOW_H
